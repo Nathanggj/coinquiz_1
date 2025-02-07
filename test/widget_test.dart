d0d186ae15bfda84d1cb2ea424112f45852e551a
giuliojonathan@gmail.com <giuliojonathan@gmail.com>
2025-02-07 16:33:52 +0100

    test

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:coinquiz/main.dart'; // Assicurati che il percorso sia corretto
+
+void main() {
+  testWidgets('Test description', (WidgetTester tester) async {
+    // Scrivi i tuoi test qui
+  });
+}
